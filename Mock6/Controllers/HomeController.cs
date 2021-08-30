550e3ace92b36b4daed39d272750fbe62101141a
clarke <cf.lamb25@gmail.com>
2021-08-30 17:40:47 -0400

    First commit for Mock6

---
+++ Mock6/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mock6.Models;
+using Mock6.Repository;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mock6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, EmployeeRepo employeeRepo, EmployeeDbContext employeeDbContext)
+        {
+            _logger = logger;
+            _employeeRepo = employeeRepo;
+            _employeeDbContext = employeeDbContext;
+        }
+        private readonly EmployeeRepo _employeeRepo;
+        private readonly EmployeeDbContext _employeeDbContext;
+
+
+        public async Task<IActionResult> Employees()
+        {
+            var model = await _employeeRepo.GetEmployeesAsync();
+            return View(model);
+        }
+
+        public IActionResult RetirementInfo(int Id)
+        {
+            var employee = _employeeDbContext.Employees.Find(Id);
+            if(employee.Age > 60)
+            {
+                ViewBag.CanRetire = true;
+            }
+            else
+            {
+                ViewBag.CanRetire = false;
+            }
+
+            ViewBag.Benefits = employee.Salary * 60/100;
+
+            return View();           
+            
+           
+        }
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mock6/Repository/EmployeeRepo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mock6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mock6.Repository
+{
+    public class EmployeeRepo
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+
+        public EmployeeRepo(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            var employees = await _employeeDbContext.Employees.ToListAsync();
+            return employees;
+        }
+
+        //public async Task<Employee> FindEmployeeAsync(int Id)
+        //{
+        //    if()
+        //}
+    }
+}
